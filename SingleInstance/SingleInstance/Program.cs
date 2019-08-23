df733d908994aaee6bde49d3902c67e0f9fcf3d3
Attila Molnar <scythianhawk@gmai.com>
2019-08-23 12:28:45 +0200

    Single Instace init

---
+++ SingleInstance/SingleInstance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SingleInstance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
