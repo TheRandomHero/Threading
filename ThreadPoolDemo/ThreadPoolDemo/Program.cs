c905daed203e22620a4daa56f91bdae8f949ce1b
Attila Molnar <scythianhawk@gmai.com>
2019-08-23 13:07:20 +0200

    ThreadingPool practice
    ThreadingPool is done

---
+++ ThreadPoolDemo/ThreadPoolDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPoolDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WaitCallback callback = new WaitCallback(ShowMyText);
+
+            ThreadPool.QueueUserWorkItem(callback, "Hi");
+            ThreadPool.QueueUserWorkItem(callback, "Ohh My G");
+            ThreadPool.QueueUserWorkItem(callback, "Geeeezuss");
+            ThreadPool.QueueUserWorkItem(callback, "wow");
+
+            Console.Read();
+
+        }
+
+        public static void ShowMyText(object state)
+        {
+            string myText = (string)state;
+
+            Console.WriteLine($"thread: {Thread.CurrentThread.ManagedThreadId}, text: {myText}");
+        }
+    }
+}
