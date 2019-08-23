db766a2cb18fabd9ea5b09b77cdc6095c5ea6daa
Attila Molnar <scythianhawk@gmai.com>
2019-08-23 12:16:40 +0200

    first task commit
    first task finished

---
+++ SimpleThreadingDemo/SimpleThreadingDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+namespace SimpleThreadingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadStart starter = new ThreadStart(Counting);
+
+            Thread firstThread = new Thread(starter);
+            Thread secondThread = new Thread(starter);
+
+            firstThread.Start();
+            secondThread.Start();
+
+            firstThread.Join();
+            secondThread.Join();
+
+            Console.Read();
+        }
+
+        public static void Counting()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Count;{i}, thread: {Thread.CurrentThread.ManagedThreadId}");
+
+
+                Thread.Sleep(10000);
+                
+            }
+        }
+    }
+}
